df20edc10e8a1370a0a175b1d365aa3ac4c6bcd3
Igor Oles <igor.oles@websensa.com>
2025-06-18 15:30:17 +0200

    currencies from param in rates handler

--- cmd/currencyapi/main.go
+++ cmd/currencyapi/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log/slog"
 	openExchange "main/internal/api/openexchange"
 	"main/internal/handlers/rates"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,4 +19,17 @@ func main() {
 
 	ratesHandler := rates.NewHandler(openExchangeAPI)
 	api.GET("/rates", ratesHandler.Handle)
+
+	slog.Info("Starting server...", slog.String("listen address", ":8080"))
+
+	srv := &http.Server{
+		Addr:              ":8080",
+		Handler:           router,
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		slog.Error("listenAndServe", slog.String("err", err.Error()))
+	}
 }
--- internal/handlers/rates/handler.go
+++ internal/handlers/rates/handler.go
@@ -1,8 +1,10 @@
 package rates
 
 import (
+	"fmt"
 	"main/internal/api"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,5 +20,34 @@ func NewHandler(currencyRateAPI api.CurrencyRate) *Handler {
 }
 
 func (h *Handler) Handle(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	resp, err := h.currencyRateAPI.GetCurrencyRates(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+
+		return
+	}
+
+	param := c.Query("currencies")
+
+	currencies := strings.Split(param, ",")
+
+	rates := make(map[string]float64)
+
+	for _, cur := range currencies {
+		val, ok := resp.Rates[cur]
+		if !ok {
+			c.JSON(
+				http.StatusBadRequest,
+				gin.H{"error": fmt.Sprintf("api do not provide %s rate", cur)},
+			)
+		}
+
+		rates[cur] = val
+	}
+
+	fmt.Printf("currencies: %+v", rates)
+
 	c.JSON(http.StatusOK, gin.H{})
 }
