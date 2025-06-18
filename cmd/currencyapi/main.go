1676aebb4b636c15d3a99c4729d2d9301287b940
Igor Oles <igor.oles@websensa.com>
2025-06-18 12:26:46 +0200

    project structure preparation

--- cmd/currencyapi/main.go
+++ cmd/currencyapi/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"main/internal/handlers/rates"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	api := router.Group("/")
+
+	ratesHandler := rates.NewHandler()
+	api.GET("/rates", ratesHandler.Handle)
+}
---
+++ internal/handlers/rates/handler.go
@@ -0,0 +1,18 @@
+package rates
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) Handle(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{})
+}
