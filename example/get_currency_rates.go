aec1e3f58d984427742c375abc9cf6d23f8420f4
Igor Oles <igor.oles@websensa.com>
2025-06-20 12:53:12 +0200

    example fix

--- example/get_currency_rates.go
+++ example/get_currency_rates.go
@@ -3,13 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"main/internal/api/openexchange"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	openExchange := openexchange.New("https://openexchangerates.org/api/")
+	_ = godotenv.Load()
 
-	resp, err := openExchange.GetCurrencyRates(context.Background(), []string{"USD"})
+	appID := os.Getenv("APP_ID")
+	if appID == "" {
+		log.Fatal(
+			"personal APP_ID for openExchangeAPI is not set." +
+				"Please set APP_ID env. Details in the README.md",
+		)
+	}
+
+	log.Println("using appID for openExchangeAPI:", appID)
+
+	openExchange := openexchange.New("https://openexchangerates.org/api/", appID)
+
+	resp, err := openExchange.GetCurrencyRates(context.Background(), []string{"USD", "INR", "EUR", "BTC"})
 	if err != nil {
 		panic(err)
 	}
